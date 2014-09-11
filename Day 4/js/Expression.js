8d1e7311a6e13c474e0d7d3890201b32aead3af0
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-10 22:32:16 -0400

    Commit 25

---
+++ Day 4/js/Expression.js
@@ -0,0 +1,44 @@
+//Brandon Hess
+//9-8-2014
+// WPF
+//Expressions
+
+//Personal
+
+console.log("1 - Personal");
+console.log("The room is 144ft by 120ft");
+var length = prompt("What is the Length of the room?");
+var width = prompt("What is the width of the room?");
+var perimeter = length*2 + width*2;
+console.log("The perimeter of the room is:"+" "+perimeter );
+alert("The Perimeter of the room is 528ft");
+
+console.log( );
+
+//Industry-Specific
+
+console.log("2 - Industy-Specific");
+console.log("The Photo is 11in by 8in");
+var pHeight = prompt("Please enter photo height");
+var pWidth = prompt("Please enter photo width");
+var iHeight =825+"pixels";
+var iWidth =500+"pixels";
+console.log("The photo is 825 pixels by 500 pixels");
+alert("The photo is 825 pixels by 500 pixels");
+
+console.log( );
+
+//Wacky
+
+console.log("3 - Wacky");
+console.log("If are level 1and for every two missions complete you level up.");
+console.log("How many missions would you have to complete to hit level 50?");
+var level = prompt("What is your current level?");
+var missions = 2;
+var answer = level * missions;
+console.log("You must complete:"+" "+ answer);
+alert("You must complete 100 missions to reach level 50");
+
+
+
+
