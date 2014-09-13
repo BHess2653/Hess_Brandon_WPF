860506e8779446555522432d1b3ce79c3cfcb2be
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-12 22:45:09 -0400

    commit 27

---
+++ Day3LabExpressions/Hess_Brandon_SliceOfPie1.js
@@ -0,0 +1,33 @@
+//Brandon Hess
+//9-8-2014
+// WPF
+//Instructional Expressions
+
+
+//Slice of Pie 1
+
+var slicesPerPizza=8;
+var peopleAtParty=16;
+var pizzasOrdered=2;
+
+var totalSlices = slicesPerPizza * pizzasOrdered;
+
+var slicesPerPerson = totalSlices / peopleAtParty;
+
+console.log("Total Slices: " + totalSlices);
+console.log("Per Person: " + slicesPerPerson);
+
+
+
+//Slice of Pie 2
+
+var slicesPerPizza=8;
+var peopleAtParty=16;
+var pizzasOrdered=2;
+
+var totalSlices = slicesPerPizza * pizzasOrdered;
+
+var sparky = totalSlices / peopleAtParty;
+
+console.log("Total Slices: " + totalSlices);
+console.log("Per Person: " + sparky);
---
+++ Day3LabExpressions/Hess_Brandon_SliceOfPie2.js
@@ -0,0 +1,19 @@
+//Brandon Hess
+//9-8-2014
+// WPF
+//Instructional Expressions
+
+
+//Slice of Pie 2
+
+var slicesPerPizza=8;
+var peopleAtParty=10;
+var pizzasOrdered=4;
+
+var totalSlices = slicesPerPizza * pizzasOrdered;
+
+var sparky = totalSlices / peopleAtParty;
+
+console.log("Total Slices: " + totalSlices);
+console.log("Per Person: " + sparky);
+
