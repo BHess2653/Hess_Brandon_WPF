e079c1d9f007eb85741e866f2e9302627f844057
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-15 19:39:56 -0400

    Commit 38

--- Day 6 class/js/main.js
+++ Day 6 class/js/main.js
@@ -27,6 +27,19 @@ var randomNumber = Math.round(Math.random()*(Number(max)-Number(min))+min);
 
 console.log(randomNumber);
 
+console.log(randomNumber);
+
+var num1 = 9.555;
+console.log(num1);
+console.log(Math.round(num1));
+//round num1
+
+console.log(Math.ceil(num1));
+//round num1 up
+
+console.log(Math.floor(num1));
+//round num1 down
+
 
 
 
