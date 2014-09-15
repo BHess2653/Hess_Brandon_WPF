aa55cf155bbafc82fd5275c4b4292f09135d6130
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-15 18:53:05 -0400

    Commit 31

--- Day 6 class/js/main.js
+++ Day 6 class/js/main.js
@@ -4,14 +4,14 @@
 
 //validating prompts
 
-var min = number(prompt("Please enter a minimum number"));
-var max = number(prompt("Please enter a maximum number"));
+var min = Number(prompt("Please enter a minimum number"));
+var max = Number(prompt("Please enter a maximum number"));
 
 //test to see that a number was actually entered!
 
 if(min===""){
     console.log("You did not enter a number!");
-    var min = number(prompt("PLEASE enter a minimum number"));
+    var min = Number(prompt("PLEASE enter a minimum number"));
 }
 
 
