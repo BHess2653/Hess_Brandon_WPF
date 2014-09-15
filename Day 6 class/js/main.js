768206e51b005c50752fea5d16b518b36029bff3
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-15 18:49:33 -0400

    commit 29

--- Day 6 class/js/main.js
+++ Day 6 class/js/main.js
@@ -4,6 +4,12 @@
 
 //validating prompts
 
+var min = number(prompt("PLease enter a minimum number"));
+var max = number(prompt("Please enter a maximum number"));
+
+//test to see that a number was actually entered!
+
+if(min==="");
 
 
 
