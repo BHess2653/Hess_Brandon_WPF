f0380be77b000438aeb76dacadaf23d81d2b053c
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-15 22:02:48 -0400

    Commit 44

--- Conditionals_Personal/js/main.js
+++ Conditionals_Personal/js/main.js
@@ -4,24 +4,56 @@
 //Conditionals
 
 //Personal
-/*
-console.log("1 - Personal");
-console.log("The room is 144ft by 120ft");
-var length = prompt("What is the Length of the room?");
-var width = prompt("What is the width of the room?");
-var perimeter = length*2 + width*2;
-console.log("The perimeter of the room is:"+" "+perimeter );
-alert("The Perimeter of the room is 528ft");
-console.log( );
-*/
-
-//Personal
 
 console.log("Personal");
+//Title
+
 alert("Perimeter of the room");
-/*alert("Note: room must be square or rectangular")*/
+//Question
+
 var length = prompt("Please enter the length of your room");
+//Prompt for length
+
 var width = prompt("Please enter the width of your room");
+//Prompt for width
+
+if(length===""){
+    console.log("You did not enter a length!");
+    length = prompt("PLEASE enter a length");
+}
+//If length is not entered it will ask again if it's still not entered then it'll ask once more
+
+if(width===""){
+    console.log("You did not enter a width!");
+    width = prompt("PLEASE enter a width");
+}
+//If width is not entered it will ask again if it's still not entered then it'll ask once more
+
+
+if(isNaN(length) || length===""){
+    length = prompt("Pease enter a length and do not leave blank");
+}
+//Prompt if you leave length blank
+
+if(isNaN(width) || width===""){
+    width = prompt("Please enter a width and do not leave blank");
+}
+//Prompt if you leave width blank
+
 var perimeter = length*2 + width*2;
+//Perimeter
+
 console.log("The Perimeter of the room is "+perimeter+"");
+//Answer in console
+
 alert("The perimeter of the room is "+perimeter+"ft");
+//Answer on browser
+
+
+
+
+
+
+
+
+
