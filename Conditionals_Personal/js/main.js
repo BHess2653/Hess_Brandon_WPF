27f46df8e666ceebb5a7e2628799679f085e612b
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-15 21:44:46 -0400

    Commit 43

--- Conditionals_Personal/js/main.js
+++ Conditionals_Personal/js/main.js
@@ -3,3 +3,25 @@
 // WPF
 //Conditionals
 
+//Personal
+/*
+console.log("1 - Personal");
+console.log("The room is 144ft by 120ft");
+var length = prompt("What is the Length of the room?");
+var width = prompt("What is the width of the room?");
+var perimeter = length*2 + width*2;
+console.log("The perimeter of the room is:"+" "+perimeter );
+alert("The Perimeter of the room is 528ft");
+console.log( );
+*/
+
+//Personal
+
+console.log("Personal");
+alert("Perimeter of the room");
+/*alert("Note: room must be square or rectangular")*/
+var length = prompt("Please enter the length of your room");
+var width = prompt("Please enter the width of your room");
+var perimeter = length*2 + width*2;
+console.log("The Perimeter of the room is "+perimeter+"");
+alert("The perimeter of the room is "+perimeter+"ft");
