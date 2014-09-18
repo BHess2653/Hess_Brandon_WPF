88f2837937f21a3407febc28be1b569f720b4159
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-17 23:08:42 -0400

    Commit 53

--- Loops_worksheet/js/main.js
+++ Loops_worksheet/js/main.js
@@ -7,62 +7,69 @@
 
 
 //While Loop
+
 console.log("  While Loop  ");
 alert("While Loop");
 console.log( );
+//Title
 
 var counter = 50;
+//start
+
 while(counter<101){
+    //total
     console.log("C");
     alert("Shields "+counter+" % and rising");
     counter++;
-
     if(counter===101){
-
         console.log("Shields fully charged!");
         alert("Shields fully charged!");
     }
-
 }
-
+//User alerts/messages
 
 console.log( );
 
-
-
-
 //Do While Loop
+
 console.log("  Do While Loop  ");
 alert("Do While Loop");
 console.log( );
 console.log(" Doctor Who ");
 alert(" Doctor Who ")
+//Title
+
 var i = 1;
+//Starting number
 do{
     console.log("Series 8 Episode " + i);
     alert("Series 8 Episode " + i);
     i++;
 }
 while(i<6);
+//Number it will stop before
+
 
 
 
 console.log( );
 
-
-
-
 //For Loop
+
 console.log("  For Loop  ");
 alert("For Loop");
 console.log( );
 console.log("Vanguard Reputation");
+//Title
+
 for(var i = 0; i < 1025; i += 25){
+    //start, End before, Number thats being added each time
 
     console.log("Current Reputation: " +i+" points");
 alert("Current Reputation: " +i+" points");
 
     if(i===1000){
+        //Message that pops up once 1000 has been reached
 
         console.log("Congratulations you've reached Vanguard rank 1!");
         alert("Congratulations you've reached Vanguard rank 1!");
