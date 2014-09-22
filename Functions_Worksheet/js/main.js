f84572a962d885abb99168aebc8b73340e9aab0e
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-22 17:34:04 -0400

    Commit 55

---
+++ Functions_Worksheet/js/main.js
@@ -0,0 +1,33 @@
+/**
+ * Created by GM_Dahaka on 9/22/14.
+ */
+
+
+
+//Circumference
+var radius = 3;
+var pi = 3.14;
+calcArea(radius, pi);
+
+function calcArea(radius, pi){
+
+    var circum = radius*2*pi;
+    console.log("The Circumference of the Circle is "+circum);
+
+    //return circum;
+}
+
+
+
+
+
+//stung
+var weight = 120;
+var sting = 8.666666667;
+calcArea(weight, sting);
+
+function calcArea(weight, sting){
+
+    var victim = sting*weight;
+    console.log("It takes "+victim+ " bee stings to kill this animal");
+}
