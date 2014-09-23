0fb9d80ea21915c5cbd24f56c88182ca695dae2b
BHess2653 <GM_Dahaka@Brandons-MacBook-Pro.local>
2014-09-22 23:04:15 -0400

    Commit 60

--- Functions_Industry/js/main.js
+++ Functions_Industry/js/main.js
@@ -5,12 +5,33 @@
 
 
 
+//Function
+alert("Meter to Millimeters");
+var meter = prompt("Please enter the amount you want converted to millimeters");
+var mil = 1000;
+console.log(meter+" meters");
+console.log(mil+" millimeters");
+console.log(meter+" X "+mil);
+
+function funInfo(meter, mil) {
+
+    var answer = meter * mil;
+    return answer;
 
 
+}if(meter===""){
+    console.log("You did not enter an amount to convert!");
+    meter = prompt("You did not enter an amount to convert!");
+
+}if(isNaN(meter) || meter===""){
+    meter = prompt("Pease enter an amount and do not leave it blank");
 
 
+}
 
+alert( meter+" meters is "+meter * mil+" millimeters");
 
+console.log(meter*mil+"mm");
 
 
 
--- Functions_Personal/js/main.js
+++ Functions_Personal/js/main.js
@@ -4,9 +4,29 @@
 //Functions Personal
 
 
+//Anonymous function
+alert("Perimeter of a Parallelogram");
+
+var base = prompt("Please enter a number for base");
+
+var side = prompt("Please enter a number for side");
+
+var parallelogram = function(b, s){
+
+    var perimeter = b*2 + s*2;
+
+    return perimeter;
 
 
+};
 
+var answer = base*2 + side*2;
+
+alert("The perimeter of the parallelogram is "+answer+"cm");
+
+var anon = parallelogram(base, side);
+
+console.log(anon);
 
 
 
